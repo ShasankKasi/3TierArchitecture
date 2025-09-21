8dec671c26fcfbe52e3c8f780f2fdffa429a5c61
Shasank Reddy <shasankreddy4563@gmail.com>
2025-09-21 13:22:47 +0530

    Destroy

--- resource/main.tf
+++ resource/main.tf
@@ -1,43 +1,42 @@
-
 module "networking" {
-  count            = var.create ? 1 : 0
-  source           = "../modules/networking"
-  vpcvar           = var.vpc_var
-  subnetvar        = var.subnetvar
-  internetgateway  = var.internetgatewayname
-  natgateway       = var.natgatewayname
-  Elasticipname    = var.Elasticipname
-  PublicRouteTableName  = var.PublicRouteTableName
-  PrivateRouteTableName = var.PrivateRouteTableName
+  count                   = try(coalesce(var.create), false) ? 1 : 0
+  source                  = "../modules/networking"
+  vpcvar                  = try(coalesce(var.vpc_var), null)
+  subnetvar               = try(coalesce(var.subnetvar), null)
+  internetgateway         = try(coalesce(var.internetgatewayname), null)
+  natgateway              = try(coalesce(var.natgatewayname), null)
+  Elasticipname           = try(coalesce(var.Elasticipname), null)
+  PublicRouteTableName    = try(coalesce(var.PublicRouteTableName), null)
+  PrivateRouteTableName   = try(coalesce(var.PrivateRouteTableName), null)
 }
 
 module "ALB" {
-  count             = var.create ? 1 : 0
+  count             = try(coalesce(var.create), false) ? 1 : 0
   source            = "../modules/ALB"
-  LoadBalancer      = var.loadbalancername
-  vpc_id            = module.networking[0].vpc_id.id
-  public_subnets_id = module.networking[0].public_subnets_id
-  ingress_rules     = var.ingress_rules
-  egress_rules      = var.egress_rules
+  LoadBalancer      = try(coalesce(var.loadbalancername), null)
+  vpc_id            = try(coalesce(module.networking[0].vpc_id.id), null)
+  public_subnets_id = try(coalesce(module.networking[0].public_subnets_id), null)
+  ingress_rules     = try(coalesce(var.ingress_rules), null)
+  egress_rules      = try(coalesce(var.egress_rules), null)
 }
 
 module "ASG" {
-  count                    = var.create ? 1 : 0
-  source                   = "../modules/ASG"
-  AutoScalingGroup         = var.asgname
-  target_group_arn         = module.ALB[0].target_group_arn
-  private_subnets_id       = module.networking[0].private_subnets_id
-  autoscalingsizeparameters= var.autoscalingsizeparameters
-  launch_template          = var.launch_template
-  cloudwatchalarm          = var.alarm
-  Scalingpolicy            = var.scaling_policy
+  count                      = try(coalesce(var.create), false) ? 1 : 0
+  source                     = "../modules/ASG"
+  AutoScalingGroup           = try(coalesce(var.asgname), null)
+  target_group_arn           = try(coalesce(module.ALB[0].target_group_arn), null)
+  private_subnets_id         = try(coalesce(module.networking[0].private_subnets_id), null)
+  autoscalingsizeparameters  = try(coalesce(var.autoscalingsizeparameters), null)
+  launch_template            = try(coalesce(var.launch_template), null)
+  cloudwatchalarm            = try(coalesce(var.alarm), null)
+  Scalingpolicy              = try(coalesce(var.scaling_policy), null)
 }
 
 module "Database" {
-  count          = var.create ? 1 : 0
+  count          = try(coalesce(var.create), false) ? 1 : 0
   source         = "../modules/Database"
-  privatesubnets = module.networking[0].private_subnets_id
-  dbsubnetname   = var.dbsubnetname
-  dbinstance     = var.dbinstance
-  dbcredentials  = var.dbcredentials
+  privatesubnets = try(coalesce(module.networking[0].private_subnets_id), null)
+  dbsubnetname   = try(coalesce(var.dbsubnetname), null)
+  dbinstance     = try(coalesce(var.dbinstance), null)
+  dbcredentials  = try(coalesce(var.dbcredentials), null)
 }
